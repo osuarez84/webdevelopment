0abe57d8aecfc5fb1501e12ab25a534c4cb1b37c
osuarez <omsulo@gmail.com>
2022-10-12 23:57:26 +0200

    Add templates.

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -14,29 +17,41 @@ const faqPage = `
 <p>A: WE have support staff answering emails 24/7 though response times may be a bit slower on weekends</p>
 `
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my big site!</h1>")
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the temple", http.StatusInternalServerError)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
+		return
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, "templates/home.gohtml")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me "+
-		"at <a href=\"mailto:omsulo@gmail.com\">omsulo@gmail.com</a>")
+	executeTemplate(w, "templates/contact.gohtml")
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, faqPage)
+	executeTemplate(w, "templates/faq.gohtml")
 }
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
-	r.NotFound(func(writer http.ResponseWriter, request *http.Request) {
-		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-	})
 	fmt.Println("Starting the server on :3000")
 	http.ListenAndServe(":3000", r)
 }
